2f9fbdc99f743c59c113910a5c24fdaf223fffc5
pejuta <sp_dragon3125@hotmail.co.jp>
2022-06-02 18:10:01 +0900

    Update SBMapItemInfo.user.js
    取得予定かけらの表示機能の追加
    かけらドロップ検索機能の追加

--- UserScripts/SBMapItemInfo.user.js
+++ UserScripts/SBMapItemInfo.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://twitter.com/11powder
 // @description Stella Boardの各種行動画面にアイテム情報を表示する。
 // @include     /^http:\/\/stella2\.428\.st\/?(?:\?mode=action)?$/
-// @version     1.0.1.1
+// @version     1.0.2
 // @updateURL   https://pejuta.github.io/SBTools/UserScripts/SBMapItemInfo.user.js
 // @downloadURL https://pejuta.github.io/SBTools/UserScripts/SBMapItemInfo.user.js
 // @grant       none
@@ -21,10 +21,33 @@ await (async () => {
         });
     }
 
+    // ロード完了時に使うことで最初に選択されている星を取得する唯一の手段となる。
+    function getCurrentStnoSelected() {
+        return parseInt(/^\d+/.exec($("#maptitle").html())[0], 10);
+    }
+
+    async function dlStarsInfo() {
+        const res = await fetch(`https://pejuta.github.io/SBTools/Data/Json/stars.json`);
+        if (!res.ok) {
+            return "dlStarsInfo faileddownloading";
+        }
+
+        return await res.json();
+    }
+
+    async function dlSignsInfo() {
+        const res = await fetch(`https://pejuta.github.io/SBTools/Data/Json/signs.json`);
+        if (!res.ok) {
+            return "dlStarsInfo faileddownloading";
+        }
+
+        return await res.json();
+    }
+
     async function dlDropLocationData() {
         const res = await fetch(`https://dl.dropboxusercontent.com/s/l8a6jz9d35aqza6/drop.json`);
         if (!res.ok) {
-            return "faileddownloading";
+            return "dlDropLocationData faileddownloading";
         }
 
         return await res.json();
@@ -33,7 +56,7 @@ await (async () => {
     async function dlClearedStarSet(eno) {
         const res = await fetch(`https://dl.dropboxusercontent.com/s/jt4ga191ul2362r/dropClear.json`);
         if (!res.ok) {
-            return "faileddownloading";
+            return "dlClearedStarSet faileddownloading";
         }
 
         const starsArr = (await res.json())[eno] || [];
@@ -42,6 +65,33 @@ await (async () => {
 
     function readSelfEno() {
         return $(".charaframeself").data("eno");
+    }
+
+    async function dlFragmentsDropInfo() {
+        const res = await fetch(`https://dl.dropboxusercontent.com/s/ir670qikmqpbjtv/fragment.json`);
+        if (!res.ok) {
+            return "dlFragmentsDropInfo faileddownloading";
+        }
+
+        return await res.json();
+    }
+
+    async function dlFragmentsToSigns() {
+        const res = await fetch(`https://dl.dropboxusercontent.com/s/48lytkxkszuurii/resolution.json`);
+        if (!res.ok) {
+            return "dlFragmentsToSigns faileddownloading";
+        }
+
+        return await res.json();
+    }
+
+    function getUAP() {
+        const $uap = $("h2.subtitle").filter((i, e) => e.innerHTML === "マップ移動").next("p").children("b").eq(2);
+        const m = /\d+$/.exec($uap.html());
+        if (!m) {
+            return -1;
+        }
+        return parseInt(m[0], 10);
     }
 
     function observeMap(moCallback) {
@@ -145,8 +195,142 @@ await (async () => {
         });
     }
 
+    function observeMapTitle(moCallback) {
+        const targetNode = document.getElementById("maptitle");
+        const moConfig = { childList: true };
+
+        const mo = new MutationObserver(moCallback);
+        mo.observe(targetNode, moConfig);
+        return mo;
+    }
+
+    function addFragmentInfoTableOnMap(stars, fragDrops, currentStno) {
+        const $table = $(`<table class="fragsdroptable"><thead><tr><th>かけら予測</th></tr></thead><tbody/></table>`);
+        const $cnt = $(`<div class="fragsdropmap"/>`).append($table);
+
+        function update(stnoSelected) {
+            const seizano = stars[stnoSelected].seizano;
+            const uap = getUAP();
+            console.log("UAP: ", uap);
+            if (uap === -1) {
+                return;
+            }
+
+            const frags = fragDrops[Math.floor(uap / 10) + seizano * 2];
+            if (!frags) {
+                return;
+            }
+            const tdHtml = "<tr><td>" + frags.join("</td></tr><tr><td>") + "</td></tr>";
+            $table.find("tbody").html(tdHtml);
+
+            const stName = stars[stnoSelected].stname;
+            $table.find("th").html(stName).attr("title", stName);
+        }
+
+        observeMapTitle((e) => {
+            const stno = getCurrentStnoSelected();
+            update(stno);
+        });
+
+        update(currentStno);
+        $cnt.insertBefore("#maparea");
+    }
+
+    function observeInputMaproute(moCallback) {
+        const targetNode = document.getElementById("inputmaproot");
+        const moConfig = { attributes: true };
+
+        const mo = new MutationObserver(moCallback);
+        mo.observe(targetNode, moConfig);
+        return mo;
+    }
+
+    function addFragmentInfoTableOnMove(stars, fragDrops, currentStno) {
+        const $table = $(`<table class="fragsdroptable"><thead><tr><th>かけら予測</th></tr></thead><tbody/></table>`);
+        const $cnt = $(`<div class="fragsdropmove"/>`).append($table);
+        const $savePointSelect = $("select[name='savepoint']");
+
+        function update() {
+            const route = $("#inputmaproot").val();
+            const isUsingDie = $("#usetype").text() === "ダイス目";
+            const savePointSelected = parseInt($savePointSelect.val(), 10);
+
+            let targetStno;
+            if (isUsingDie) {
+                const splt = route.split(",");
+                targetStno = parseInt(splt[splt.length - 1], 10);
+            } else if (savePointSelected > 0) {
+                targetStno = savePointSelected;
+            } else {
+                targetStno = currentStno;
+            }
+
+            const seizano = stars[targetStno].seizano;
+            let uap = getUAP();
+            if (uap === -1) {
+                return;
+            }
+            if (isUsingDie) {
+                uap++;
+            }
+
+            const frags = fragDrops[Math.floor(uap / 10) + seizano * 2];
+            if (!frags) {
+                $table.find("tbody").empty();
+                return;
+            }
+            const tdHtml = "<tr><td>" + frags.join("</td></tr><tr><td>") + "</td></tr>";
+            $table.find("tbody").html(tdHtml);
+
+            const stName = stars[targetStno].stname;
+            $table.find("th").html(stName).attr("title", stName);
+        }
+
+        $savePointSelect.on("change", update);
+        observeInputMaproute(update);
+        update();
+        $cnt.prependTo($("#d1").closest("p"));
+    }
+
+    function addFragmentFinder(signs, fragDrops, fragToSigns) {
+        const uap = getUAP();
+        const $cnt = $(`<div class="fragfinder"><div><p class="selecttargetsign">かけらを探す：<select>--</select></p><p class="selectedsign"></p><p class="possiblefrags"></p></div></div>`);
+        const $select = $cnt.find("select");
+
+        const f2sOfCurrentUAP = fragToSigns[Math.floor(uap / 10)];
+
+        $select.html(`<option selected value="-1"></option><option>${Object.keys(f2sOfCurrentUAP).join("</option><option>")}</option>`);
+
+        const $signsCell = $cnt.find(".selectedsign");
+        const $possibleFragsCell = $cnt.find(".possiblefrags");
+
+        $select.on("change", () => {
+            $possibleFragsCell.empty();
+
+            if ($select.val() === "-1") {
+                $signsCell.empty();
+                return;
+            }
+
+            const fragSelected = $select.children(":selected").html();
+            $signsCell.html(`<span>${f2sOfCurrentUAP[fragSelected].join("</span><span>")}</span>`);
+        })
+
+        $signsCell.on("click", "span", function(e) {
+            $(e.currentTarget).addClass("selected").removeClass("halftp").siblings().removeClass("selected").addClass("halftp");
+
+            const signSelected = e.currentTarget.innerHTML;
+            const frags = fragDrops[Math.floor(uap / 10) + signs[signSelected] * 2];
+            $possibleFragsCell.html(`<span>${frags.join("</span><span>")}</span>`);
+        });
+
+        $cnt.prependTo($("#d1").closest("p"));
+    }
+
+
+
     function addNotice() {
-        $("#maparea + p").after(`<p><small>※マップ上の未取得アイテムの強調アイコンは、そのアイテムの取得後すぐには消滅しません。Libra Reportの更新タイミング(朝/夕方)に更新が行われます。</small></p>`);
+        $("#maparea + p").after(`<p><small>※マップ上の未取得アイテムの強調アイコンは、そのアイテムの取得後すぐには消滅しません。<a href="http://gameprn.web.fc2.com/SB2" target="_blank">Libra Report</a>の更新タイミング(朝/夕方)に更新が行われます。</small></p>`);
     }
 
     $("head").append(
@@ -259,16 +443,108 @@ await (async () => {
         display: block;
         background-color: rgb(255,255,255);
     }
+
+    .fragsdropmap {
+        position: relative;
+        z-index: 400;
+    }
+
+    .fragsdropmove {
+        position: relative;
+    }
+
+    .fragsdropmap > .fragsdroptable {
+        position: absolute;
+        top: 150px;
+        left: 50px;
+    }
+    .fragsdropmove > .fragsdroptable {
+        position: absolute;
+        top: -24px;
+        right: 310px;
+    }
+    .fragsdroptable {
+        border-collapse: separate;
+        border-spacing: 0px 1px;
+        text-align:center;
+    }
+    .fragsdroptable th {
+        width: 120px;
+        max-width: 120px;
+        background-color: rgb(191,191,255);
+        padding: 0px 6px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        color: rgb(0,7,39);
+    }
+    .fragsdroptable td {
+        background-color: rgb(63,63,63);
+        padding: 2px 6px;
+    }
+
+    .fragfinder {
+        position: relative;
+    }
+    .fragfinder > div {
+        position: absolute;
+        top: -35px;
+        left: 720px;
+        width: 320px;
+        max-width: 320px;
+        min-height: 240px;
+        border-radius: 4px;
+        border: 2px solid #72729b;
+    }
+    .selecttargetsign {
+        font-weight: bold;
+        background-color: rgb(63 63 23);
+        padding: 0px 6px;
+    }
+    .selectedsign > span {
+        cursor: pointer;
+        display: inline-block;
+        border-radius: 4px;
+        background-color: rgb(79,79,111);
+        margin: 1px 4px 1px 0px;
+        padding: 1px 2px;
+        border: 1px solid transparent;
+    }
+    .selectedsign > span.selected {
+        border: 1px solid rgb(255,255,159);
+    }
+    .selectedsign > span.halftp {
+        opacity: 0.3;
+    }
+    .possiblefrags {
+        padding-left: 10px;
+    }
+    .possiblefrags > span {
+        display: inline-block;
+        border-radius: 4px;
+        background-color: rgb(95,63,63);
+        margin: 1px 4px 1px 0px;
+        padding: 1px 2px;
+    }
 </style>`);
 
     await (async () => {
+        const currentStno = getCurrentStnoSelected();
         const eno = readSelfEno();
-        const locationToItem = await dlDropLocationData();
-        const clearedStars = await dlClearedStarSet(eno);
-        updateMapOnMutation(locationToItem, clearedStars);
-        includeItemInfoOnMap(locationToItem, clearedStars);
-        appendItemIcon(locationToItem, clearedStars);
-        addToggleTooltipsButton(clearedStars);
+        const locationToItem = dlDropLocationData();
+        const clearedStars = dlClearedStarSet(eno);
+        const stars = dlStarsInfo();
+        const signs = dlSignsInfo();
+        const fragDrops = dlFragmentsDropInfo();
+        const fragToSigns = dlFragmentsToSigns();
+
+        updateMapOnMutation(await locationToItem, await clearedStars);
+        includeItemInfoOnMap(await locationToItem, await clearedStars);
+        appendItemIcon(await locationToItem, await clearedStars);
+        addToggleTooltipsButton(await clearedStars);
+        // addFragmentInfoTableOnMap(await stars, await fragDrops, currentStno);
+        addFragmentInfoTableOnMove(await stars, await fragDrops, currentStno);
+        addFragmentFinder(await signs, await fragDrops, await fragToSigns);
         addNotice();
         $("#maparea").on("mouseenter", ".layout[id^=stno]", function (e) {
             showTooltip(e.currentTarget);
